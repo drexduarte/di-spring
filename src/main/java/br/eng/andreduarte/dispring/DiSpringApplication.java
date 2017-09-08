79fddf2a090fe20c9cdabfcfea9fb7bb11ad8413
Andre Duarte <drexduarte@gmail.com>
2017-09-08 10:42:25 -0300

    First commit.

---
+++ src/main/java/br/eng/andreduarte/dispring/DiSpringApplication.java
@@ -0,0 +1,12 @@
+package br.eng.andreduarte.dispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/br/eng/andreduarte/dispring/DiSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.eng.andreduarte.dispring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
